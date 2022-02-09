d720769b71b363eb9fa55240d7473779a4e1c81a
Aniket Kanere <aniket15304@gmail.com>
2022-02-09 16:21:38 +0530

    Updated axios

--- client/src/axios.config.js
+++ client/src/axios.config.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://simple-social-app-production.up.railway.app',
   responseType: 'json',
 });
 
