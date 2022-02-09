e6424414ef7214e648f8579b416f8ee8a5f14093
Aniket Kanere <aniket15304@gmail.com>
2022-02-09 18:02:56 +0530

    Updated env

--- server/models/index.js
+++ server/models/index.js
@@ -7,10 +7,10 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-console.log("config: ",config);
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(config.use_env_variable, config);
 } else {
   sequelize = new Sequelize(
     config.database,
