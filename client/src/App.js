d3d18b15deb77544d4adb80f1f58021c5482eb48
anikxt <aniket15304@gmail.com>
2023-03-04 13:53:58 +0530

    Fix dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
