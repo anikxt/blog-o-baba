203026f6edff82348484faaf0d17d785cfe6a14a
anikxt <aniket15304@gmail.com>
2023-03-04 14:23:11 +0530

    Fix dependency

--- client/src/axios.config.js
+++ client/src/axios.config.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 const axios = Axios.create({
   baseURL: 'https://simple-social-app-production.up.railway.app',
+  // baseURL: 'http://localhost:8080',
   responseType: 'json',
 });
 
